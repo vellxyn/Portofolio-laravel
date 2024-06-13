27563b5e7662dee97d6258870f760de2d017b3d8
vellxyn <marvelmaleakhisugiarto@gmail.com>
2024-06-13 13:50:50 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My Portfolio</title>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="libs/font-awesome/css/font-awesome.min.css">
+    <link href="bootstrap.min.css" rel="stylesheet">
+    <link href="styles.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="mobile-menu-open" class="shadow-large">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+    </div>
+    <!-- End #mobile-menu-toggle -->
+    <header>
+        <div id="mobile-menu-close">
+            <span>Close</span> <i class="fa fa-times" aria-hidden="true"></i>
+        </div>
+        <ul id="menu" class="shadow">
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#experience">Pengalaman</a>
+            </li>
+            <li>
+                <a href="#education">Tentang SMK Wikrama</a>
+            </li>
+            <li>
+                <a href="#projects">Projects</a>
+            </li>
+            <li>
+                <a href="#skills">Skills</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+        </ul>
+    </header>
+    <!-- End header -->
+
+    <div id="lead">
+        <div id="lead-content">
+            <h1>4iam.vimoraa 💫
+                Marvel
+            </h1>
+            <h2>Software Engineer</h2>
+            <a href="#" class="btn-rounded-white">Hello</a>
+        </div>
+        <!-- End #lead-content -->
+
+        <div id="lead-overlay"></div>
+
+        <div id="lead-down">
+            <span>
+                <i class="fa fa-chevron-down" aria-hidden="true"></i>
+            </span>
+        </div>
+        <!-- End #lead-down -->
+    </div>
+    <!-- End #lead -->
+
+    <div id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h2 class="heading">Tentang Aku</h2>
+                </div>
+                <div class="col-md-8">
+                    <p>
+                    Haiii....
+                    Perkenalkan namaku Marvel Maleakhi Sugiarto, usian ku dini berada di 16 tahun, aku lahir di kota Jepara, pada bulan dan tanggal : 19 Maret 2008, pada aku kecil aku suka banget bis, saat makan aku harus di bawa keterminal bis untuk melihat bis jika tidak aku tidak mau makan, pada 2012 aku pindah tinggal ke kota bogor, saat di bogor aku mulai sekolah dari TK, SD, SMP, dan sampai SMK, SMK aku bersekolah di SMK Wikrama Kota Bogor dan aku duduk di bangku kelas 10, aku memasuki jurusan PPLG atau rekayasa perangkat lunak dan GIM, pada ini aku mulai memasuki laravel, dan akan menaiki kelas 11, jujur di wikrama ini sekolah sangat seru, banyak yang bertanya tanya, kenapa wikrama tidak boleh membawa HP kesekolah????.. jawabannya supaya setiap siswa dapat mendengarkan materi yang guru jelasin dan tidak sibuk dengan HP nya masing masing, begini pengalaman dan penjelasan saya mohon maaf bila salah kata. TerimaKasih🙏🏻
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End #about -->
+
+    <div id="experience" class="background-alt">
+        <h2 class="heading">Pengalaman</h2>
+        <div id="experience-timeline">
+            <div data-date="September 2015 – September 2016">
+                <h3>Masuk Wikrama</h3>
+                <h4>Penuh Perjuangan</h4>
+                <p>
+                Masuk Wikrama bukan hal mudah bagi ku, karena perlu mengejar nilai bagus dari smp sampai lulus, hingga pada akhir nya aku daftar di SMK Wikrama Bogor, saat pendaftaraan aku di tanya tanya tentang keahlian ku, mengenai public speaking ku, dan hobby serta cita-cita yang akan di gapai, sampai pada akhir nya aku bisa masuk sesuai dengan jurusan yang aku ingin kan.
+                </p>
+            </div>
+
+            <div data-date="September 2015 – September 2016">
+                <h3>Menjuarai Basket</h3>
+                <h4>Bangga akan diri sendiri</h4>
+                <p>
+                Awal aku tidak bisa bermain basket, setelah aku masuk wikrama aku latihan dan gabung team basket wikrama, aku fikus dengan latihan ku, dan pada waktunya ada kejuaraan BSI Basketball, pada akhir nya Wikrama mampu menjuarai tim dan mendapatkan juara ke 1, serta mendapatkan sertivicat, mendali, serta piala yang di pajang di sekolah.
+                </p>
+            </div>
+
+        </div>
+    </div>
+    <!-- End #experience -->
+
+    <div id="education">
+        <h2 class="heading">Tentang SMK Wikrama</h2>
+        <div class="education-block">
+            <h3>SMK Wikrama Kota Bogor</h3>
+            <span class="education-date">2023-2024</span>
+            <h4>Sekolah Informatika</h4>
+            <p>
+            SMK Wikrama Bogor didirikan oleh Ir. Itasia Dina Sulvianti dan Dr.H.RP Agus Lelana dibawah naungan Yayasan Prawitama pada tahun 1996 di bekas gudang KUD. Kompetensi keahlian yang pertama dibuka pada saat itu adalah sekretaris dengan jumlah hanya 34 siswa.
+            </p>
+        </div>
+        <!-- End .education-block -->
+
+        <div class="education-block">
+            <h3>SMK Wikrama Kota Bogor</h3>
+            <span class="education-date">2023-2024</span>
+            <h4>Jurusan Yang Ada di SMK Wikrama Bogor</h4>
+            <ul>
+                <li>
+                SMK Wikrama Bogor membuka 7 kompetensi keahlian, diantaranya (1) Otomatisasi dan Tata Kelola Perkantoran; (2) Teknik Komputer dan Jaringan; (3) Rekayasa Perangkat Lunak; (4) Multimedia; (5) Bisnis Daring dan Pemasaran; (6) Tata Boga; dan (7) Perhotelan.
+                </li>
+            </ul>
+        </div>
+        <!-- End .education-block -->
+    </div>
+    <!-- End #education -->
+
+    
+                    <!-- End .project-info -->
+                </div>
+                <!-- End .project -->
+            </div>
+        </div>
+    </div>
+    <!-- End #projects -->
+
+    <div id="skills">
+        <h2 class="heading">Skills</h2>
+        <ul>
+            <li>JavaScript</li>
+            <li>CSS</li>
+            <li>PHP</li>
+            <li>HTML</li>
+            <li>Laravel</li>
+
+        </ul>
+    </div>
+    <!-- End #skills -->
+
+    <div id="contact">
+        <h2>Get in Touch</h2>
+        <div id="contact-form">
+            <form method="POST" action="https://formspree.io/email@email.com">
+                <input type="hidden" name="_subject" value="Contact request from personal website" />
+                <input type="email" name="_replyto" placeholder="Your email" required>
+                <textarea name="message" placeholder="Your message" required></textarea>
+                <button type="submit">Send</button>
+            </form>
+        </div>
+        <!-- End #contact-form -->
+    </div>
+    <!-- End #contact -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-5 copyright">
+                    <p>
+                        Copyright &copy; <span id="current-year">2024</span>  4iam.vimoraa
+                    </p>
+                </div>
+                <div class="col-sm-2 top">
+                    <span id="to-top">
+                        <i class="fa fa-chevron-up" aria-hidden="true"></i>
+                    </span>
+                </div>
+                <div class="col-sm-5 social">
+                    <ul>
+                        <li>
+                            <a href="https://github.com/" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://stackoverflow.com/" target="_blank"><i class="fa fa-stack-overflow" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://linkedin.com/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://plus.google.com/" target="_blank"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End footer -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/scripts.min.js"></script>
+</body>
+
+</html>
